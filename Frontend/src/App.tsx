4c09aa6e26f575b8fa7cb9cd8a6d2a62629be9b2
Peheliya <hwpeheliya@gmail.com>
2024-09-19 13:12:46 +0530

    Implement routes for each pages

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,12 +1,23 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Login from './pages/Login';
+import Signin from './pages/Signin';
 
 function App() {
 
   return (
-    <>
-      <h1>Hello World!</h1>
-    </>
+    <BrowserRouter>
+    <Routes>
+      <Route path="home" element={<Home/>}/>
+      <Route path="about" element={<About/>}/>
+      <Route path="contact" element={<Contact/>}/>
+      <Route path="login" element={<Login/>}/>
+      <Route path="signin" element={<Signin/>}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
-export default App
+export default App;
